43f6fb6e5c2f6381dd94b658cc25efc4886bde22
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-06-15 17:37:59 -0500

    add styling and mediaqueries

---
+++ pages/home/index.js
@@ -0,0 +1,13 @@
+const socials = {
+  links: {
+    youtube: 'https://www.youtube.com/channel/UCNTeMcd7BDOuNrVf1yRGZlA',
+    github: 'https://github.com/frog1123',
+    spotify: 'https://open.spotify.com/user/ckenvyd4xokwet9bbir3hfxn9?si=bbda1b6ab230463b',
+    steam: 'https://steamcommunity.com/id/frogdude1123'
+  }
+};
+
+document.getElementById('_github').onclick = () => window.open(socials.links.github, '_blank');
+document.getElementById('_youtube').onclick = () => window.open(socials.links.youtube, '_blank');
+document.getElementById('_spotify').onclick = () => window.open(socials.links.spotify, '_blank');
+document.getElementById('_steam').onclick = () => window.open(socials.links.steam, '_blank');
